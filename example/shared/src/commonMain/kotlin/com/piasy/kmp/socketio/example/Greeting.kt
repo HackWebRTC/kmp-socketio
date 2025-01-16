87cdbdfc488256b89175939fae4bc02ae5c09379
Piasy <xz4215@gmail.com>
2025-01-16 08:53:31 +0800

    fix example

--- example/shared/src/commonMain/kotlin/com/piasy/kmp/socketio/example/Greeting.kt
+++ example/shared/src/commonMain/kotlin/com/piasy/kmp/socketio/example/Greeting.kt
@@ -1,7 +1,5 @@
 package com.piasy.kmp.socketio.example
 
-import com.piasy.kmp.socketio.logging.Logger
-import com.piasy.kmp.socketio.logging.LoggerInterface
 import com.piasy.kmp.socketio.socketio.IO
 import com.piasy.kmp.socketio.socketio.Socket
 import io.ktor.util.date.GMTDate
@@ -12,20 +10,6 @@ import kotlinx.io.bytestring.unsafe.UnsafeByteStringOperations
 class Greeting {
     @OptIn(UnsafeByteStringApi::class)
     fun greet() {
-        Logger.setLogger(object : LoggerInterface {
-            override fun debug(tag: String, log: String) {
-                TODO("Not yet implemented")
-            }
-
-            override fun info(tag: String, log: String) {
-                TODO("Not yet implemented")
-            }
-
-            override fun error(tag: String, log: String) {
-                TODO("Not yet implemented")
-            }
-        })
-
         IO.socket("http://172.16.11.25:3000", IO.Options()) { socket ->
             socket.on(Socket.EVENT_CONNECT) { args ->
                 println("on connect ${args.joinToString()}")
