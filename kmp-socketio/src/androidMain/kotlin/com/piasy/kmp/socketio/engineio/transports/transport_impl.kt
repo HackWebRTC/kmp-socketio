bf32fbc73813e001e65664614a8031b847549f14
Piasy <xz4215@gmail.com>
2025-01-28 16:38:35 +0800

    delete unused androidMain dir

--- kmp-socketio/src/androidMain/kotlin/com/piasy/kmp/socketio/engineio/transports/transport_impl.kt
+++
@@ -1,9 +0,0 @@
-package com.piasy.kmp.socketio.engineio.transports
-
-import io.ktor.client.HttpClientConfig
-import io.ktor.client.HttpClient
-import io.ktor.client.engine.okhttp.OkHttp
-
-actual fun httpClient(config: HttpClientConfig<*>.() -> Unit): HttpClient = HttpClient(OkHttp) {
-    config(this)
-}
